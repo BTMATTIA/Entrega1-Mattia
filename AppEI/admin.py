a9b301637905f7683e8783d1c515c5e52aac58ca
BTMATTIA <120423175+BTMATTIA@users.noreply.github.com>
2023-01-06 04:30:34 -0300

    Delete AppEI directory

--- AppEI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Terror, Comedia, Accion
-
-# Register your models here.
-
-admin.site.register(Terror)
-admin.site.register(Comedia)
-admin.site.register(Accion)
